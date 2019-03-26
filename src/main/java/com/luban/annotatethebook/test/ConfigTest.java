6e46a490d2dfeb8f9a53a2daf3b82a257c433123
huangfu <huangfusuper@163.com>
2019-03-26 13:38:10 +0800

    增加返回扫描包下的所有Class类

--- src/main/java/com/luban/annotatethebook/test/ConfigTest.java
+++ src/main/java/com/luban/annotatethebook/test/ConfigTest.java
@@ -10,8 +10,7 @@ public class ConfigTest {
     public static void main(String[] args) throws ClassNotFoundException {
         AnnotationConfigApplicationContext annotationConfigApplicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        String tYpePath = annotationConfigApplicationContext.getTYpePath();
-        System.out.println(tYpePath);
+        annotationConfigApplicationContext.getTYpePath();
 
     }
 
--- src/main/java/com/luban/util/AnnotationConfigApplicationContext.java
+++ src/main/java/com/luban/util/AnnotationConfigApplicationContext.java
@@ -4,6 +4,8 @@ import com.luban.annotatethebook.anno.ComponentScan;
 
 import java.io.File;
 import java.lang.annotation.Annotation;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 自定义版本的注解扫描
@@ -11,6 +13,7 @@ import java.lang.annotation.Annotation;
  */
 public class AnnotationConfigApplicationContext {
     private Class target;
+    private String fullyQualifiedName;
     public AnnotationConfigApplicationContext(Class target) {
         this.target = target;
     }
@@ -19,18 +22,60 @@ public class AnnotationConfigApplicationContext {
         if(target.isAnnotationPresent(ComponentScan.class)){
             ComponentScan componentScanValue = (ComponentScan) target.getAnnotation(ComponentScan.class);
             String packagePath = componentScanValue.value();
+            fullyQualifiedName = packagePath;
             String path = packagePath.replaceAll("\\.","/" );
             String rootPackagePath = this.getClass().getResource("/").getPath();
             String targetPackagePath = rootPackagePath+path;
             File file = new File(targetPackagePath);
-            String[] list = file.list();
-            for (String s : list) {
-                System.out.println(s);
+            List<String> list = new ArrayList<String>();
+            list = getPaths(file, list);
+            List<String> fullyQualifiedNames = getFullyQualifiedName(list);
+            List<Class> classArray = getClassArray(fullyQualifiedNames);
+            for (Class aClass : classArray) {
+                System.out.println(aClass.getSimpleName());
             }
 
         }else{
             throw new CannotFindAnnotations("找不到"+ComponentScan.class+"注解");
         }
         return null;
+    }
+
+    public List<String> getPaths(File filePath,List<String> paths){
+        File[] files = filePath.listFiles();
+        if(files.length == 0){
+            return null;
+        }
+        for (File file : files) {
+            if(file.isDirectory()){
+                getPaths(file,paths);
+            }else{
+                paths.add(file.getPath());
+            }
+        }
+        return paths;
+    }
+
+    public List<String> getFullyQualifiedName(List<String> list){
+        List<String> fullyQualifiedNames = new ArrayList<String>();
+        for (String name : list) {
+            String s = name.replaceAll("\\\\", "\\.");
+            String substring = s.substring(s.indexOf(fullyQualifiedName), (s.length() - 6));
+            fullyQualifiedNames.add(substring);
+        }
+        return fullyQualifiedNames;
+    }
+
+    public List<Class> getClassArray(List<String> list){
+        List<Class> classes = new ArrayList<Class>();
+        for (String fullyQualifiedName : list) {
+            try {
+                Class clazz = Class.forName(fullyQualifiedName);
+                classes.add(clazz);
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+        return classes;
     }
 }
